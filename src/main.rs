5caf2e81ffd8e8030b0445b5cc821f15bc68d568
suxiqiang <suxiqiang@gzrsic.com>
2022-10-05 15:50:55 +0800

    feat: 添加异步库，定时刷新打印。

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 mod sys_info_monitor;
+use tokio::time;
+use sys_info_monitor::common;
 
-fn main(){
-    sys_info_monitor::common::print_sys_info();
+#[tokio::main]
+async fn main(){
+    
+    let mut interval = time::interval(time::Duration::from_secs(2));
+    let mut sys = common::init_sys_instance();
+    common::print_disk(&sys);
+    common::print_memory(&sys);
+    loop {
+        interval.tick().await;
+        common::sys_refresh(&mut sys);
+        common::print_network(&mut sys);
+        common::print_this_process(&sys).ok();
+    }
 }
--- src/sys_info_monitor/common.rs
+++ src/sys_info_monitor/common.rs
@@ -1,18 +1,26 @@
 // https://docs.rs/sysinfo/0.26.2/sysinfo/
-
-use sysinfo::{DiskExt, System, SystemExt,ProcessExt};
+#![allow(dead_code)]
+use sysinfo::{DiskExt, System, SystemExt,ProcessExt,NetworkExt};
 // 监控系统性能，用于方便控制本程序的读写IO，网络IO，CPU等，剩余磁盘空间不能占用过多，
+
+pub fn init_sys_instance()->System{
+    System::new_all()
+}
+
+pub fn sys_refresh(sys:&mut System){
+    sys.refresh_all();
+}
+
 pub fn print_sys_info(){
     let mut sys = System::new_all();
     sys.refresh_all();
     print_disk(&sys);
     print_memory(&sys);
     print_this_process(&sys).ok();
-
 }
 
 // 打印磁盘信息
-fn print_disk(sys:&System){
+pub fn print_disk(sys:&System){
     // We display all disks' information:
     println!("=> disks:");
     for disk in sys.disks() {
@@ -21,7 +29,7 @@ fn print_disk(sys:&System){
 }
 
 // 打印内存信息
-fn print_memory(sys:&System){
+pub fn print_memory(sys:&System){
     println!("=> system:");
     // RAM and swap information:
     println!("total memory: {} MB", byte_to_million_byte(sys.total_memory()));
@@ -31,13 +39,34 @@ fn print_memory(sys:&System){
 }
 
 // 打印当前进程的信息
-fn print_this_process(sys:&System)->Result<(),String>{
+pub fn print_this_process(sys:&System)->Result<(),String>{
     let pid = sysinfo::get_current_pid()?;
     let process =  sys.process(pid).ok_or("没找到本序的Pid。")?;
-    println!("{}，使用内存：{} MB,CUP：{}%",  process.name(),byte_to_million_byte(process.memory()),process.cpu_usage());
+    // NOTE: 磁盘读写 在windows上好像有点问题。读一直为同一个值，写一直为0。
+    let disk_usage = process.disk_usage();
+
+    println!("{}，使用内存：{} MB,CUP：{:.2}%",  
+        process.name(),
+        byte_to_million_byte(process.memory()),
+        process.cpu_usage()
+    );
+    println!("磁盘 读：{} B，写{} B ",disk_usage.total_read_bytes,disk_usage.total_written_bytes);
     return  Ok(());
 }
 
-fn byte_to_million_byte(val:u64)->u64{
+// 打印网络状态
+pub fn print_network(sys:&System){
+    let networks = sys.networks();
+    println!("网络状态:");
+    for (interface_name, network) in networks {
+        println!("{} 收：{} B，发:{} B", 
+            interface_name,
+            network.received(),
+           network.transmitted()
+        );
+    }
+}
+
+pub fn byte_to_million_byte(val:u64)->u64{
     return val/1024/1024;
 }
